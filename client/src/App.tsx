8e58079a469a12a4fb21aca86374f2cc3daa5133
lenargim <linz14@mail.ru>
2025-04-09 07:57:12 +0700

    me rerender

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Routes, useNavigate} from "react-router-dom";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/SignUp/SignUp";
