565c22a7d4228c34d5a3e861eba9aeb46cc32c68
lenargim <linz14@mail.ru>
2025-08-05 04:07:04 +0700

    price for sqft

--- client/src/Components/CustomPart/CustomPartStandardDoorForm.tsx
+++ client/src/Components/CustomPart/CustomPartStandardDoorForm.tsx
@@ -82,7 +82,7 @@ export default CustomPartStandardDoorForm;
 
 export const getCustomPartStandardDoorPrice = (doors: MaybeNull<StandardDoorAPIType[]>, name: string, color:string): number => {
     if (!doors) return 0;
-    const glassPrice: number = name !== 'standard-doors' ? 10 : 0;
+    const glassPrice: number = name !== 'standard-doors' ? 30 : 0;
     const colorPrice: number = color !== 'Default White' ? 30 : 0;
     return doors.reduce((acc, door) => {
         const sqr = door.width * door.height / 144;
