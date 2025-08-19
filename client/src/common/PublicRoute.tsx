21168102bd500f5296c378be4e15636a8cb75f56
lenargim <linz14@mail.ru>
2025-08-19 15:54:32 +0700

    navigate to profile if has token

--- client/src/common/PublicRoute.tsx
+++ client/src/common/PublicRoute.tsx
@@ -1,13 +1,14 @@
 import {Navigate, Outlet} from "react-router-dom";
-import {useAppSelector} from "../helpers/helpers";
-import React from "react";
+import {useAppDispatch, useAppSelector} from "../helpers/helpers";
+import React, {useEffect} from "react";
 import Loading from "./Loading";
+import {loadUser} from "../store/reducers/userSlice";
 
 export default function PublicRote() {
+    const token = localStorage.getItem('token')
     const user = useAppSelector(state => state.user.user);
     const loading = useAppSelector(state => state.user.loading);
-
     if (loading) return <Loading />;
-    if (user) return <Navigate to="/profile" />;
+    if (user || token) return <Navigate to="/profile" />;
     return <Outlet />;
 };
